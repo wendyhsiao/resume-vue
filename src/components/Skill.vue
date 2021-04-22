ec894984f439d3ac411a00d8b705c933b682e831
wendy.hsiao <wendy19940331@gmail.com>
2021-04-23 01:25:37 +0800

    [修改] 技能區樣式

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,62 +1,44 @@
 <template>
-  <div class="skill-group">
-    <div class="skill-bg"></div>
-    <h1>技能 Skill</h1>
-    <div class="skill-list">
-      <div class="skill-item">
-        <p>前端</p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>Bootstrap</li>
-          <li>Vue.js</li>
-        </ul>
-      </div>
-      <div class="skill-item">
-        <p>後端</p>
-        <ul>
-          <li>Node.js / Express.js</li>
-          <li>MySQL / Sequelize</li>
-          <li>MongoDB / Mongoose</li>
-          <li>RESTful API</li>
-        </ul>
-      </div>
-      <div class="skill-item">
-        <p>其他</p>
-        <ul>
-          <li>Photoshop</li>
-          <li>Illustrator</li>
-        </ul>
+  <div class="wrapper bg-color">
+    <div class="skill-group vw-80">
+      <h2>技能 Skill</h2>
+      <div class="skill-list">
+        <div class="skill-item">
+          <p>前端</p>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Bootstrap</li>
+            <li>Vue.js</li>
+          </ul>
+        </div>
+        <div class="skill-item">
+          <p>後端</p>
+          <ul>
+            <li>Node.js / Express.js</li>
+            <li>MySQL / Sequelize</li>
+            <li>MongoDB / Mongoose</li>
+            <li>RESTful API</li>
+          </ul>
+        </div>
+        <div class="skill-item">
+          <p>其他</p>
+          <ul>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .skill-bg {
-    width: 100%;
-    height: 40vh;
-    background:  url('../../public/img/writing-in-notebook.jpg') no-repeat top 100% center/cover fixed;
-    margin-bottom: 3em;
-  }
   .skill-group {
-    width: 100vw;
-    height: 100vh;
     text-align: center;
-    line-height: 2em;
-    color: #333;
-  }
-  .skill-group h1 {
-    width: 80vw;
-    margin: 0 auto;
-    font-size: 1.5em;
-    border-bottom: solid 2px #101010;
-    margin-bottom: 1em;
   }
   .skill-list {
-    width: 80vw;
-    margin: 0 auto;
     display: flex;
     align-items: stretch;
     justify-content: center;
