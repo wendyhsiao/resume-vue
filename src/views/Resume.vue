0a6bde83a2be70b16f4c2d4a89220d94f32e7092
wendy.hsiao <wendy19940331@gmail.com>
2021-04-26 02:39:05 +0800

    [加入] Top 區

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Top />
     <SelfIntroductio />
     <Skill />
     <Project />
@@ -10,6 +11,7 @@
 </template>
 
 <script>
+import Top from '../components/Top.vue'
 import SelfIntroductio from '../components/SelfIntroductio.vue'
 import Skill from '../components/Skill.vue'
 import Project from '../components/Project.vue'
@@ -19,6 +21,7 @@ import Footer from '../components/Footer.vue'
 
 export default {
   components: {
+    Top,
     SelfIntroductio,
     Skill,
     Project,
